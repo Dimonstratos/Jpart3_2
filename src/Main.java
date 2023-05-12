ff1699d1c5f0ba1a1c7ee56cd594be77f535ecb1
Demitros <serrasalmus.80@gmail.com>
2023-05-12 14:26:47 +0500

    added src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         int client = 100; // на счете клиента
-        int i = 100; // сумма пополнения счета
+        int i = 1001; // сумма пополнения счета
 
         int bonus;
         if (i > 1000) {
